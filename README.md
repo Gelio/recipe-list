9291d55fb9c1b75d8dd7bc9b2ac9eba316b7c38a
Gelio <voreny.gelio@gmail.com>
2016-02-11 21:43:36 +0100

    initial commit, set up environment

---
+++ README.md
@@ -0,0 +1,3 @@
+# Recipe list #
+
+A simple recipe list app devloped in React, that allows users to add, modify and delete recipes.
---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+}
+
+ReactDOM.render(<Layout />, document.getElementById('app'));
